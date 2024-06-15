145b691bee585f51cfd50b2019e20d9d2db03572
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-06-15 11:02:05 +0000

    recursion 02 complete

---
+++ Basic recursion/02_recurssion.cpp
@@ -0,0 +1,41 @@
+// ----------------------------type name N times
+
+// #include <iostream>
+// using namespace std; 
+ 
+// void printName(int i, int n) {
+
+//     if(i == n + 1) return; 
+
+//     cout << "Aditya" << endl; 
+//     printName(i+1, n); 
+// }
+
+// int main() {
+
+//     printName(1, 5); 
+//     return 0; 
+// }
+
+
+// --------------------Type N to 1-------------------------------------------
+// #include<iostream>
+// using namespace std; 
+
+// int count = 0;
+// int a;  
+// void printN(int i, int n) {
+
+//     if (i < 1 ) return; 
+
+//     cout << i << endl; 
+//     printN(i-1, n); 
+// }
+
+// int main() {
+
+//     printN(3,3);
+//     return 0; 
+// }
+
+
