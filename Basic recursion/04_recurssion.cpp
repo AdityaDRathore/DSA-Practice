2b934dc06146ea8e190f67f9e1aa41b30517f4c4
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-06-15 16:55:12 +0000

    is string a Palindrome check complete

--- Basic recursion/04_recurssion.cpp
+++ Basic recursion/04_recurssion.cpp
@@ -1,23 +1,44 @@
 //-------------------------------Reverse an array-----------------------------------------
 
+// #include<iostream>
+// using namespace std; 
+
+// void reverseArr(int i, int arr[], int n) {
+
+//     if(i >= n/2) return; 
+
+//     swap(arr[i], arr[n-i-1]); 
+//     reverseArr(i+1, arr, n); 
+// }
+
+// int main(){
+//     int arr[5] = {1,3,4,6,2}; 
+//     reverseArr(0,arr,5); 
+
+//     for(int i = 0; i < 5; i++) {
+//         cout << arr[i] << " "; 
+//     }
+
+//     return 0; 
+// }
+
+
+//-----------------------------------Is a Palindrome--------------------------------------
+
 #include<iostream>
 using namespace std; 
 
-void reverseArr(int i, int arr[], int n) {
+bool isPalindrome(int i, int s[], int n) {
 
-    if(i >= n/2) return; 
+    if(i >= n/2) return true; 
+    if(s[i] != s[n-i-1]) return false; 
 
-    swap(arr[i], arr[n-i-1]); 
-    reverseArr(i+1, arr, n); 
-}
+    isPalindrome(i+1, s, n); 
+} 
 
-int main(){
-    int arr[5] = {1,3,4,6,2}; 
-    reverseArr(0,arr,5); 
+int main() {
 
-    for(int i = 0; i < 5; i++) {
-        cout << arr[i] << " "; 
-    }
-
+    int s[5] = {'M','A','D','S','M'}; 
+    cout << isPalindrome(0, s, 5) << endl; 
     return 0; 
 }
