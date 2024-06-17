910ef98cbaebfca6b9333ac8ae6c567003b1d946
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-06-17 13:13:02 +0000

    hash done using map STL in string

--- Hashing/01_Hashing.cpp
+++ Hashing/01_Hashing.cpp
@@ -56,32 +56,57 @@
 
 //------------------------------------------------------Hasing of array(numericals) using Map----------------------------------------------------------------
 
+// #include<iostream>
+// #include<bits/stdc++.h>
+// using namespace std; 
+
+// int main() {
+//     int n; 
+//     cin >> n; 
+//     int arr[n]; 
+//     for(int i = 0; i < n; i++) {
+//         cin >> arr[i]; 
+//     }
+
+//     map<int, int> mp; 
+//     for(int i = 0; i < n; i++) {
+//         mp[arr[i]]++;
+//     }
+
+//     int q; 
+//     cin >> q; 
+//     while(q--) {
+//         int number; 
+//         cin >> number; 
+//         cout << mp[number] << endl; 
+//     }
+
+//     return 0;
+// }
+
+
+//---------------------------------------------------------Hasing using MAP on string-----------------------------------------------------------------------
+
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std; 
 
 int main() {
-    int n; 
-    cin >> n; 
-    int arr[n]; 
-    for(int i = 0; i < n; i++) {
-        cin >> arr[i]; 
-    }
-
-    map<int, int> mp; 
-    for(int i = 0; i < n; i++) {
-        mp[arr[i]]++;
+    string s; 
+    cin >> s; 
+    
+    map<char, int> mp;
+    for(int i = 0; i < s.length(); i++) {
+        mp[s[i] - 'a']++; 
     }
 
     int q; 
     cin >> q; 
     while(q--) {
-        int number; 
-        cin >> number; 
-        cout << mp[number] << endl; 
+        char c; 
+        cin >> c; 
+        cout << mp[c - 'a'] << endl; 
     }
 
     return 0; 
-
-
 }
