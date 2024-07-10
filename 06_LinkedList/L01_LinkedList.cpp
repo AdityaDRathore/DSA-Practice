aa31fc4c9dc1b6533f78dea26a63078356f7531f
Aditya Rathore <153427299+AdityaDRathore@users.noreply.github.com>
2024-07-10 12:55:06 +0000

    Linked List node output

--- 06_LinkedList/L01_LinkedList.cpp
+++ 06_LinkedList/L01_LinkedList.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<bits/stdc++.h>
+#include<vector>
 using namespace std; 
 
 struct Node {
@@ -7,14 +9,25 @@ struct Node {
     Node* next;
 
     public:
-    Node(int date1, Node*next1) {
-        date = date1; 
-        next = next1; 
+    Node(int date1) {
+        date = date1;
+        next = nullptr;  
     } 
 };
 
+Node* convertArrtoLL(vector<int>&arr) {
+    Node* head = new Node(arr[0]); 
+    Node* mover = head; 
+    for(int i = 1; i < arr.size(); i++) {
+        Node*temp = new Node(arr[i]); 
+        mover->next = temp; 
+        mover = temp; 
+    }
+    return head; 
+}
+
 int main() {
-    int arr[4] = {2,4,7,8}; 
-    Node*y = new Node(arr[2], nullptr); 
-    cout << y << endl; 
+    vector<int> arr = {2,4,7,8}; 
+    Node* head = convertArrtoLL(arr); 
+    cout << head->date << endl; 
 }
